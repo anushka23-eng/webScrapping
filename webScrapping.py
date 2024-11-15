1459c7fc2e5654dbd1d7c20ac4a2267bc9084d41
anushka23-eng <ajha34515@gmail.com>
2024-11-15 06:53:54 -0800

    Update webScrapping.py
    second commit

--- webScrapping.py
+++ webScrapping.py
@@ -1,38 +1,3 @@
-# from bs4 import BeautifulSoup
-# import requests
-# import pandas as pd
-
-# relDigital = requests.get('https://www.jumia.co.ke/all-products/')
-
-# name_info = []
-# price_info = []
-# rating_info = []
-
-
-# for page in range(1, 51):
-#     url = "https://www.jumia.co.ke/all-products/" + \
-#         "?page=" + str(page) + "#catalog-listing"
-#     furl = requests.get(url)
-#     jsoup = BeautifulSoup(furl.content, 'html.parser')
-#     products = jsoup.find_all('div', class_='info')
-
-#     for product in products:
-#         name = product.find('h3', class_='name').text.replace('\n', '')
-#         price = product.find('div', class_='prc').text.replace('\n', '')
-
-#         try:
-#             rating = product.find(
-#                 'div', class_='stars_s').text.replace('\n', '')
-#         except:
-#             rating = 'None'
-
-#         name_info.append(name)
-#         price_info.append(price)
-#         rating_info.append(rating)
-
-#         print(name_info, price_info, rating_info)
-
-
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -40,35 +5,27 @@ import time
 
 base_url = "https://www.amazon.in/s?k=laptop&page={}"
 
-# Headers to mimic a browser request
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
 }
 
-# empty list to store product data
 products = []
 
-# Scrapping of first 40 pages
 for page in range(1, 41):
     url = base_url.format(page)
     print(f"Scraping page {page}...")
 
-    # Get the page content
     response = requests.get(url, headers=headers)
 
-    # If the response status is not 200 (OK), skip the page
     if response.status_code != 200:
         print(f"Failed to retrieve page {page}")
         continue
 
-    # Parse the HTML content
     soup = BeautifulSoup(response.content, "html.parser")
 
-    # Find all product containers
     product_divs = soup.find_all(
         "div", {"data-component-type": "s-search-result"})
 
-    # Loop through each product container
     for product in product_divs:
         try:
             # Extract product name
